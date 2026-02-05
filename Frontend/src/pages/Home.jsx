575366768280b6e5ce6b62a1ad323f5c5b679e67
Rohan-Saxena644 <rohsax83@gmail.com>
2026-02-05 22:43:21 +0530

    Delete the commented code

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,11 +1,3 @@
-// export default function Home() {
-//   return (
-//     <div className="text-center mt-20">
-//       <h1 className="text-4xl font-bold">Wanderlust</h1>
-//       <p className="mt-4 text-lg">Find your next stay</p>
-//     </div>
-//   );
-// }
 
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
